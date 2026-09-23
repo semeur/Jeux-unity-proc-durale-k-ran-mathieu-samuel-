f76371955717825d027a9d34f105d8f417e32471
BOURGOGNE Mathieu <m.bourgogne@rubika-edu.re>
2026-09-23 16:48:09 +0400

    i'll learn from my mystake

--- Assets/Scipt/Génération_laby.cs
+++ Assets/Scipt/Génération_laby.cs
@@ -2,29 +2,14 @@ using UnityEngine;
 
 public class Génération_laby : MonoBehaviour
 {
-    public GameObject mur;
-
-    [SerializeField]
-    private GameObject mur_e;
-
-    [SerializeField]
-    private GameObject mur_n;
-    
-    [SerializeField]
-    private GameObject mur_s;
-
-    [SerializeField]
-    private GameObject non_visitee;
-
-    public bool is_visited { get; private set; }
-
-    public void Visit()
+    public GameObject case;
+    private void Start()
     {
-        for (int i = 0; i < 1000; i+=110)
+        for (int x = 0; x < 1001;  x+= 100)
         {
-            for (int j = 0; j < 1000; j+=110)
+            for (int j = 0; j < 1001; j += 100)
             {
-                Instantiate(mur, new Vector3(i, 0, j), Quaternion.identity);
+                
             }
         }
     }
