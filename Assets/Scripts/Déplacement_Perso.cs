3fffdd90ae4d6dfa3c3145cc93835e7c4d86be76
Mahot keran <k.mahot@rubika.local>
2026-09-23 12:58:04 +0400

    Ajout de la téléportation du personnage (uniquement pendant le devs)

--- Assets/Scripts/Déplacement_Perso.cs
+++ Assets/Scripts/Déplacement_Perso.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Déplacement_Perso : MonoBehaviour
 {
@@ -7,22 +8,22 @@ public class Déplacement_Perso : MonoBehaviour
     public KeyCode back = KeyCode.DownArrow;
     public KeyCode left = KeyCode.LeftArrow;
     public KeyCode right = KeyCode.RightArrow;
+    public KeyCode respawn = KeyCode.R; //objectif de cette ligne est de faire respawn le personnage a des coordonnées précis
     public bool verif = true;
+    MeshRenderer Cube; //La meshrender du Cube
 
     public float speed = 5.5f; //vitesse de déplacement du personnage
     public Vector3 mouvement = Vector3.zero; //différent mouvement que le joueur peut faire
 
     void Start()
-    {
-        float deltaTime = Time.deltaTime; //représente le temps écoulé depuis la frame d'avant.
-        float deltaMove = speed * deltaTime; // un calcul avec ma vitesse à la l.11 et avec le temps écoulé depuis la frame d'avant. 
+    { 
     }
 
     // Update is called once per frame
     void Update()
     {
-        float deltaTime = Time.deltaTime;
-        float deltaMove = speed * deltaTime;
+        float deltaTime = Time.deltaTime; //représente le temps écoulé depuis la frame d'avant.
+        float deltaMove = speed * deltaTime; // un calcul avec ma vitesse à la l.11 et avec le temps écoulé depuis la frame d'avant.
 
 
         mouvement = Vector3.zero;
@@ -37,17 +38,33 @@ public class Déplacement_Perso : MonoBehaviour
             mouvement += Vector3.left;
 
         if (Input.GetKey(right))
+        {
             mouvement += Vector3.right;
+        }
         if (verif == false)
+        {
             mouvement += Vector3.down;
-        
+        }
+
+
+
+
+
+
 
 
         PlayerMove.Move(mouvement * deltaMove);
+        if (Input.GetKey(respawn) && transform.position.y < 0.5f)
+        {
+            print("Je suis revenu"); //test du if 
+            transform.position = new Vector3(1f, 0.5f, 1f);
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
         verif = true;
+        Cube = other.GetComponent<MeshRenderer>(); // rend le cube "invisible" lors du contact
+        Cube.enabled = false;
     }
     private void OnTriggerExit(Collider other)
     {
