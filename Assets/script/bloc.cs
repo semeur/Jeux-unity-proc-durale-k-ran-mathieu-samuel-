ad2523ef3ed71dfdb913688468b2c70102903791
samuel moustaquima <s.moustaquima@rubika-edu.re>
2026-09-24 11:29:33 +0400

    version 1.5

--- Assets/script/bloc.cs
+++ Assets/script/bloc.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class bloc : MonoBehaviour
 {
@@ -13,42 +14,51 @@ public class bloc : MonoBehaviour
 
      MeshRenderer cube_haut_renderer;
      MeshRenderer cube_bas_renderer;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+
+    void Awake()
     {
+         //obtenir les info sur la "couleur " des compossant du bloc quand on les appels 
          cube_haut_renderer = cube_haut.GetComponent<MeshRenderer>();
          cube_bas_renderer = cube_bas.GetComponent<MeshRenderer>();
     }
 
-    // Update is called once per frame
+    /*Ne sert a rien pour le moment
     void Update()
     {
         
     }
+    */
 
-    public void changer_en_hebe ()
+    public void changer_en_hebe ()//le biome plaine est choisie et va etre générer
     { 
+        //on garde les 2partie du bloc pour avoir la couleur de l'heurbe et la couleur de la terre
         cube_haut.SetActive(true);
         cube_bas.SetActive(true);
 
+        //ici on change les couleur pour bien correpondre au bloc
         cube_haut_renderer.material.color = herbematerial.color;
         cube_bas_renderer.material.color = Terrematerial.color;
 
     }
-    public void changer_en_sable()
+    public void changer_en_sable()//le biome desert est choisie et va etre générer
     {
+        //on garde que la partie basse car on n'a pas besoin de faire different coulleur
         cube_haut.SetActive(false);
         cube_bas.SetActive(true);
 
+        //ici on change les couleur pour bien correpondre au bloc mais on change quand même les 2 partie au cas ou l'autre s'affiche quand même
         cube_haut_renderer.material.color = sablematerial.color;
         cube_bas_renderer.material.color = sablematerial.color;
 
     }
-    public void changer_en_roche()
+    public void changer_en_roche()//le biome montagne est choisie et va etre générer
     {
+        //on garde que la partie basse car on n'a pas besoin de faire different coulleur
         cube_haut.SetActive(false);
         cube_bas.SetActive(true);
 
+        //ici on change les couleur pour bien correpondre au bloc mais on change quand même les 2 partie au cas ou l'autre s'affiche quand même
         cube_haut_renderer.material.color = rochematerial.color;
         cube_bas_renderer.material.color = rochematerial.color;
 
--- Assets/script/spawner.cs
+++ Assets/script/spawner.cs
@@ -4,73 +4,51 @@ public class spawner : MonoBehaviour
 {
     //pour ajouter les differente partie des bloc
     public GameObject cube_entier;
-
+    [SerializeField] int radom_biome;
 
     //pour generer le choix de biome
     //public int radom_biome = 0;
 
 
-
-
     void Start()
     {
         //va réaliser l'action a l'intérieur en boucle temps que x et z ne sont pas = a 100 , donc il va crée 400 bloc au total
-        for (float x = 0f; x < 400; x++)
+        for (float x = 0f; x < 200; x++)
         {
-            for (float z = 0f; z < 400; z++)
+            for (float z = 0f; z < 200; z++)
             {
-                //obtenir les info sur la "couleur " des compossant du bloc
-
 
                 //lance choix du biome au hasard parmis 3 option , 0 = plaine || 1 = desert || 2 = montagne
-                int radom_biome = Random.Range(0, 3);
+                radom_biome = Random.Range(0, 3);
                 
                 if (radom_biome == 0)//faire apparaitre la partie haute de l'herbe et la teindre en vert et faire apparaitre la partie basse de l'herbe et la teindre en marron
                 { 
-                    
+                  
                     float PerlinY = Mathf.PerlinNoise(x * 0.05f, z * 0.05f) * 3;
                     GameObject bloc = Instantiate(cube_entier, new Vector3(z * 2, PerlinY, x * 2), Quaternion.identity);
                     bloc.GetComponent<bloc>().changer_en_hebe();
-
+                
                 }
 
-                if (radom_biome == 1)//faire disparaitre la partie haute et la teindre en jaune au cas ou et faire apparaitre la partie basse de l'herbe et la teindre en jaune
+                else if (radom_biome == 1)//faire disparaitre la partie haute et la teindre en jaune au cas ou et faire apparaitre la partie basse de l'herbe et la teindre en jaune
                 {
 
                     float PerlinY = Mathf.PerlinNoise(x * 0.05f, z * 0.05f) * -10;
-                    Instantiate(cube_entier, new Vector3(z * 2, PerlinY, x * 2), Quaternion.identity);
+                    GameObject bloc = Instantiate(cube_entier, new Vector3(z * 2, PerlinY, x * 2), Quaternion.identity);
                     bloc.GetComponent<bloc>().changer_en_sable();
 
                 }
 
-                if (radom_biome == 2)//faire disparaitre la partie haute et la teindre en gris au cas ou et faire apparaitre la partie basse de l'herbe et la teindre en gris
+                else if (radom_biome == 2)//faire disparaitre la partie haute et la teindre en gris au cas ou et faire apparaitre la partie basse de l'herbe et la teindre en gris
                 {
-
+                    
                     float PerlinY = Mathf.PerlinNoise(x * 0.05f, z * 0.05f) * 50;
-                    Instantiate(cube_entier, new Vector3(z * 2, PerlinY, x * 2), Quaternion.identity);
+                    GameObject bloc = Instantiate(cube_entier, new Vector3(z * 2, PerlinY, x * 2), Quaternion.identity);
                     bloc.GetComponent<bloc>().changer_en_roche();
-
+                
                 }
-
+                
             }
         }
     }
 }
-
-
-
-// Update is called once per frame || ne sert a rien pour le moment
-    /*
-    void biome_chox()
-    {
-        haut player = other.GetComponent<haut>();
-
-        // 1 = plaine || 2 = desert || 3 = montagne
-        if (radom_biome == 0)
-        {
-            SetActive
-
-        }
-    }
-    */
-  
